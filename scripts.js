00f2e616163707f57d85dfed05bb08f56ac88ba5
Prub <seifeddineweldca@gmail.com>
2024-06-11 17:44:52 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// tooltip
+var tooltip = document.querySelectorAll(".tooltip1");
+
+document.addEventListener("mousemove", fn, false);
+
+function fn(e) {
+  for (var i = tooltip.length; i--; ) {
+    tooltip[i].style.left = e.pageX + "px";
+    tooltip[i].style.top = e.pageY + "px";
+  }
+}
+// ..
