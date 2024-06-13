e7ce35f562e67152fa89b37a62dcf6df2209aeb7
Prub <seifeddineweldca@gmail.com>
2024-06-13 17:10:11 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,43 @@ function fn(e) {
   }
 }
 // ..
+
+/* Toggle Unmute */
+
+function toggleMute() {
+  var video = document.getElementById("bgvid");
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+}
+
+/* Delay */
+
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+/* Show Video */
+
+function showVideo() {
+  var element = document.getElementById("bgvid");
+  var button = document.getElementById("button");
+  element.style.display = "block";
+  button.style.display = "none";
+  delay(100).then(() => toggleMute());
+}
+// ..
+
+function mutevid() {
+  var video = document.getElementById("bgvid");
+  var icon = document.getElementById("icon");
+  if (video.muted) {
+    video.muted = false;
+    icon.className = "fa-sharp fa-solid fa-volume-high";
+  } else {
+    video.muted = true;
+    icon.className = "fa-solid fa-volume-xmark";
+  }
+}
