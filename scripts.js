ffbca63b3cae6fb26e4e1e5ef250c63734bb4ca0
Prub <seifeddineweldca@gmail.com>
2024-07-08 10:34:19 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -36,6 +36,8 @@ function showVideo() {
   element.style.display = "block";
   button.style.display = "none";
   delay(100).then(() => toggleMute());
+  delay(50).then(() => element.play());
+  element.volume = 0.8;
 }
 // ..
 
